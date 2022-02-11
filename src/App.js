5a02766c3722588bad30f10215565d4118b836cc
Amine <34844409+freemanbot@users.noreply.github.com>
2022-02-11 10:38:06 +0100

    Removed unused components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
-import Toast from "react-bootstrap/Toast";
+import React from "react";
+
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+
 import "./App.css";
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-  return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
-};
+
 const App = () => (
   <Container className="p-3">
     <Container className="p-5 mb-4 bg-light rounded-3">
